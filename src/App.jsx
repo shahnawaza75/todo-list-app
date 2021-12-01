5d6855eb932530689d3010bbc4373083bcdc9bc6
shahnawaz ali <shahnawaza75@gmail.com>
2021-12-01 21:19:48 +0530

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+
   return (
     <div className='App'>
       <h1>Hello there</h1>
+      <button onClick= { ()=> setCount(count += 1 )} > {count} </button>
     </div>
   )
 }
