d0b57f8ce511a4590e91b8ee5e82178beaccdc24
Joel <joelangeltw@gmail.com>
2021-12-08 15:15:34 -0500

    changes on the card and card layout

--- src/components/Content/Card/Card.jsx
+++ src/components/Content/Card/Card.jsx
@@ -1,11 +1,32 @@
+import { useState } from 'react'
+
 import './Card.css'
+import Options from '../Icons/Options'
+import OptionsContent from '../OptionsContent/OptionsContent'
 
 export default function Card() {
+  const [optionsActive, setOptionsActive] = useState(false)
+
   return (
     <div className='card'>
-      <h2>Title</h2>
-      <p>Content</p>
-      <p>State</p>
+      <div className='card__header'>
+        <h2 className='card__title black'>Title</h2>
+        <button
+          className='card__button'
+          onClick={() => {
+            setOptionsActive(!optionsActive)
+          }}
+        >
+          <Options />
+        </button>
+      </div>
+      <p className='card__content text-regular grayish'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur et
+        earum quos libero! Neque veritatis commodi molestiae nulla qui
+      </p>
+      <p className='card__state text-medium black'>State</p>
+
+      {optionsActive && <OptionsContent />}
     </div>
   )
 }
